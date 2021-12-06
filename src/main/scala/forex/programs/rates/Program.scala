57f23be6cdaef205931c1c454dccd700c53a5718
arbitrary-dev <arbitrary-dev@users.noreply.github.com>
2021-12-06 14:06:10 +0200

    update TODO's

--- src/main/scala/forex/programs/rates/Program.scala
+++ src/main/scala/forex/programs/rates/Program.scala
@@ -10,12 +10,14 @@ import forex.services._
 
 import errors._
 
+// TODO ProgramSpec
+// TODO test a case where requests for all pairs made after cache expiration
+//      24*60/5*(9^2) = 23328 requests/day
 class Program[F[_]: Monad](
     ratesService: RatesService[F],
     cacheService: CacheService[F],
 ) extends Algebra[F] {
 
-  // TODO test in ProgramSpec
   override def get(request: Protocol.GetRatesRequest): F[Error Either Rate] = {
     val pair = Pair(request.from, request.to)
     (for {
--- src/main/scala/forex/services/package.scala
+++ src/main/scala/forex/services/package.scala
@@ -7,5 +7,6 @@ package object services {
   type CacheService[F[_]] = cache.Algebra[F]
   final val CacheServices = cache.Interpreters
 
-  // TODO BatchServices
+  // TODO BatchServices (using fs2.Stream)
+  //      To utilize OneFrame API's possibility of querying several pairs at once.
 }
