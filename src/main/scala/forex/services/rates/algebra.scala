84b2dfb6c3f06fb13e8948e9912ac22577ff671d
arbitrary-dev <arbitrary-dev@users.noreply.github.com>
2022-01-08 15:57:23 +0200

    add support to request several rates at once

--- src/main/scala/forex/services/rates/algebra.scala
+++ src/main/scala/forex/services/rates/algebra.scala
@@ -1,8 +1,11 @@
 package forex.services.rates
 
 import forex.domain.Rate
+import fs2.Stream
+
 import errors._
 
 trait Algebra[F[_]] {
   def get(pair: Rate.Pair): F[Error Either Rate]
+  def get2(pairs: Rate.Pair*): Stream[F, Error Either Rate] = ???
 }
